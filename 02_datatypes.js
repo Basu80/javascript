ed0f44eb4327c3aed9ffe4342c98c4b6a61bc4dc
Basu80 <basukshitiz30@gmail.com>
2026-05-31 13:30:59 +0545

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+//alert(3 + 3) //we are using nodejs not browser
+console.log(3 + 3)
+//code readability should be high
+console.log("basu")
+let name = "Kshitiz" 
+ let age = 19
+let isLoggedIn = false
+let state 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned till  
+// symbol => uniqueness 
+//object
+console.log(typeof "Kshitiz");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
