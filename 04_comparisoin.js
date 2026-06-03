4e03a1881fd98163cacb5fff76a0c09d11c0cb36
Basu80 <basukshitiz30@gmail.com>
2026-06-03 23:04:05 +0545

    comparisons in js

---
+++ 04_comparisoin.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1) //basic conversion
+// console.log(2 >= 1)
+
+
+// this type of comparison must be avoided and write clean code 
+console.log("2" > 1) //different dattatypes 
+console.log("02" > 1)
+
+console.log(null > 0)  //null case or zero case 
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+
+// ===
+console.log("2" === 2);
