e55c74599d544459d1f00796fd2e0c5e4b546ebf
Basu80 <basukshitiz30@gmail.com>
2026-06-04 16:39:28 +0545

    updated

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -29,3 +29,19 @@ const myFunction = function(){
 }
 console.log(typeof bigNumber)
 console.log(typeof myFunction);
+
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object*/
+       
